7e3d9a9bc60e44d96953093fa0b1bc3397ca7813
Jennings Zhang <jenni_zh@protonmail.com>
2020-09-13 00:55:37 -0400

    docker manifest rm multiple args
    Signed-off-by: Jennings Zhang <jenni_zh@protonmail.com>

--- cli/command/manifest/rm.go
+++ cli/command/manifest/rm.go
@@ -1,37 +1,47 @@
 package manifest
 
 import (
+	"strings"
+
 	"github.com/docker/cli/cli"
 	"github.com/docker/cli/cli/command"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
-type rmOpts struct {
-	target string
-}
-
 func newRmManifestListCommand(dockerCli command.Cli) *cobra.Command {
-	opts := rmOpts{}
-
 	cmd := &cobra.Command{
-		Use:   "rm MANIFEST_LIST",
-		Short: "Delete a manifest list from local storage",
-		Args:  cli.ExactArgs(1),
+		Use:   "rm MANIFEST_LIST [MANIFEST_LIST...]",
+		Short: "Delete one or more manifest lists from local storage",
+		Args:  cli.RequiresMinArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			opts.target = args[0]
-			return runRm(dockerCli, opts)
+			return runRm(dockerCli, args)
 		},
 	}
 
 	return cmd
 }
 
-func runRm(dockerCli command.Cli, opts rmOpts) error {
-
-	targetRef, err := normalizeReference(opts.target)
-	if err != nil {
-		return err
+func runRm(dockerCli command.Cli, targets []string) error {
+	var errs []string
+	for _, target := range targets {
+		targetRef, refErr := normalizeReference(target)
+		if refErr != nil {
+			errs = append(errs, refErr.Error())
+			continue
+		}
+		_, searchErr := dockerCli.ManifestStore().GetList(targetRef)
+		if searchErr != nil {
+			errs = append(errs, searchErr.Error())
+			continue
+		}
+		rmErr := dockerCli.ManifestStore().Remove(targetRef)
+		if rmErr != nil {
+			errs = append(errs, rmErr.Error())
+		}
 	}
-
-	return dockerCli.ManifestStore().Remove(targetRef)
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, "\n"))
+	}
+	return nil
 }
